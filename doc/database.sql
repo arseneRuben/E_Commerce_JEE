3f51611a2166d9fcc90afc71cf636edd702c336b
isi <isi@751FJW2>
2022-11-30 14:10:26 -0500

    Projet : creation tables bd

--- doc/database.sql
+++ doc/database.sql
@@ -1,10 +1,23 @@
-CREATE TABLE if not exists site (id INT(11), name VARCHAR(50), adress VARCHAR(50), description LONGTEXT);
+
+DELATE DATABASE visites;
+CREATE DATABASE if not exists  visites;
+CREATE TABLE if not exists site (id INT(11), name VARCHAR(50), adress VARCHAR(50), description LONGTEXT, price DECIMAL(4,2), image1 VARCHAR(100), image2 VARCHAR(100));
 ALTER TABLE site 
 ADD PRIMARY KEY(id);
 
-CREATE TABLE if not exists user (id INT(11), email VARCHAR(50), password VARCHAR(100), role INT(1));
+INSERT INTO SITE( name, adress, description, price, image1, image2) VALUES 
+("Grande roue Montreal", "362 Rue de la Commune E, Montréal, QC H2Y 0B4" , "28,74", "granderou1.jpg", "granderou1.jpg");
+
+
+CREATE TABLE if not exists user (id INT(11), email VARCHAR(50), password VARCHAR(100), role INT(1), gender  INT(1));
+
 ALTER TABLE user 
-ADD PRIMARY KEY(id);
+ADD PRIMARY KEY(id)
+ADD UNIQUE KEY UNIQUE_EMAIL (email);
+
+INSERT INTO USER( email, password, role, gender) VALUES 
+("ruben@gmail.com", "passwordruben" , 1,1);
+
 
 CREATE TABLE if not exists comment (id INT(11), content LONGTEXT, createdAd datetime, role INT(1), user_id INT(11), site_id INT(11));
 ALTER TABLE comment 
