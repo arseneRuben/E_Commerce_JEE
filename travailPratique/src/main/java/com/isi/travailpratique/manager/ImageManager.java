5f8a6d0fc380932ce7c07fe9d8800057acd7e17c
2210098 <2210098@isi-montreal.com>
2022-12-08 18:43:46 -0500

    TP

--- travailPratique/src/main/java/com/isi/travailpratique/manager/ImageManager.java
+++ travailPratique/src/main/java/com/isi/travailpratique/manager/ImageManager.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.isi.travailpratique.manager;
 
-/**
- *
- * @author isi
- */
-public class ImageManager {
+public class ImageManager extends Manager {
     
 }
