5833f8e65bb0dcac1cb8ad0f659c79bf9c39404f
isi <isi@751FJW2>
2022-12-08 14:48:42 -0500

    update

---
+++ travailPratique/src/main/java/com/isi/travailpratique/model/Manager.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.isi.travailpratique.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author isi
+ */
+public abstract class Manager {
+    protected static String dbName="contactdb";
+    protected static String urlServeur = "jdbc:mariadb://localhost:3306/" + dbName;
+    protected static String username = "root";
+    protected static String password = "abc123...";
+    protected static Connection connexion;
+    protected  PreparedStatement statement;
+    protected  ResultSet result;
+    protected String query;
+    
+    
+    
+   
+
+}
