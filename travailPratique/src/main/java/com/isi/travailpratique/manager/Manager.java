e7df30e6d2401f43256568f956f3579262260905
2210098 <2210098@isi-montreal.com>
2022-12-11 11:50:20 -0500

    CommandManager

--- travailPratique/src/main/java/com/isi/travailpratique/manager/Manager.java
+++ travailPratique/src/main/java/com/isi/travailpratique/manager/Manager.java
@@ -1,28 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.isi.travailpratique.manager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-/**
- *
- * @author isi
- */
 public abstract class Manager {
-    protected static String dbName="visites";
-    protected static String urlServeur = "jdbc:mariadb://localhost:3310/" + dbName;
+
+    protected static String dbName = "visites";
+    protected static String urlServeur = "jdbc:mariadb://localhost:3306/" + dbName;
     protected static String username = "root";
     protected static String password = "abc123...";
     protected static Connection connexion;
-    protected  PreparedStatement statement;
-    protected  ResultSet result;
+    protected PreparedStatement statement;
+    protected ResultSet result;
     protected String query;
-    
-    
-   
 
 }
